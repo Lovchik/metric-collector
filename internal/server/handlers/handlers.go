558ff5e3445e0bc13d4af5ac040f4109e433256f
Vitaly Louchy <vitaly.louchy@indev.by>
2025-03-10 16:36:11 +0300

    Review comments fix

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -2,9 +2,10 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"metric-collector/internal/server/services"
 )
 
-func MetricRouter(router *gin.RouterGroup, s *Service) {
+func MetricRouter(router *gin.RouterGroup, s *services.Service) {
 	router.POST("update/:type/:name/:value", s.UpdateMetric)
 	router.GET("value/counter/:name", s.GetCounter)
 	router.GET("value/gauge/:name", s.GetGauge)
