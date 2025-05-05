e557f7e0fe10e03c6e5f58ffffaefe764e897527
Vitaly Louchy <vitaly.louchy@indev.by>
2025-05-05 16:36:22 +0300

    tests

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -7,9 +7,9 @@ import (
 )
 
 func MetricRouter(router *gin.RouterGroup, s *services.Service) {
-	router.POST("/update", middleware.AuthMiddleware(), middleware.LoggerMiddleware(), middleware.GzipMiddleware(), s.UpdateMetricViaJSON)
+	router.POST("/update/", middleware.AuthMiddleware(), middleware.LoggerMiddleware(), middleware.GzipMiddleware(), s.UpdateMetricViaJSON)
 	router.POST("/updates", middleware.AuthMiddleware(), middleware.LoggerMiddleware(), middleware.GzipMiddleware(), s.UpdateMetrics)
-	router.POST("/value", middleware.AuthMiddleware(), middleware.LoggerMiddleware(), middleware.GzipMiddleware(), s.GetMetric)
+	router.POST("/value/", middleware.AuthMiddleware(), middleware.LoggerMiddleware(), middleware.GzipMiddleware(), s.GetMetric)
 	router.GET("", middleware.LoggerMiddleware(), s.GetAllMetrics)
 	router.POST("/update/:type/:name/:value", middleware.AuthMiddleware(), middleware.LoggerMiddleware(), middleware.GzipMiddleware(), s.UpdateMetric)
 	router.GET("/value/counter/:name", middleware.LoggerMiddleware(), s.GetCounter)
